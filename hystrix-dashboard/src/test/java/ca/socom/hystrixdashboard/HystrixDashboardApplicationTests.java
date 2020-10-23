1ab422a378cbe58892aa46a23b77c1ab932dd221
dorolyci4 <dorolyci4@DESKTOP-D627793>
2020-10-23 00:49:37 -0400

    First Commit

---
+++ hystrix-dashboard/src/test/java/ca/socom/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.socom.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
